0572a721c35ddda42388384c50f346f7f8aed7ed
Chetan Satpute <chetan.satpute2002@gmail.com>
2023-01-29 16:38:24 +0530

    Login Form Assignment

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, StyleSheet} from 'react-native';
+
+import LoginForm from './src/components/LoginForm';
 
 function App() {
   return (
-    <SafeAreaView>
-      <Text>Hello World!</Text>
+    <SafeAreaView style={styles.container}>
+      <LoginForm />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#ffffff',
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
 
 export default App;
